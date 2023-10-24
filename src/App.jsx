83917489b8fe33a4057d3636de7164787d695fad
Carlos Barrios <barriosb.carlosd@gmail.com>
2023-10-24 11:09:32 -0400

    Se agrego la funcionalidad de props en App.jsx para las experiencias de usuario

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,51 @@
-import { useState } from 'react'
+import { useState } from "react";
 
-import './App.css'
-import NavBar from './components/NavBar';
-import Section from './components/Section';
-import Experiences from './components/Experiences';
+import "./App.css";
+import NavBar from "./components/NavBar";
+import Section from "./components/Section";
+import Experiences from "./components/Experiences";
+import Morocha from "./assets/images/Morocha.jpg";
+import Blondie from "./assets/images/Bloondie.png"
+import Guy from "./assets/images/Guy.jpg"
 
 function App() {
-  
-
   return (
     <>
       <NavBar />
       <Section />
-      <Experiences />
+      <div className="container mx-auto px-4">
+        <div class="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-6 gap-2">
+          <Experiences 
+            name="Morocha" 
+            img={Morocha}
+            rating="5.0"
+            reviewCount={6}
+            country= "USA"
+            title="Life lessons with "
+            price={136}
+          />
+          <Experiences 
+          name="Blondie" 
+          img={Blondie}
+          rating="4.0"
+          reviewCount={10}
+          country= "RD"
+          title="Bet for it with "
+          price={200}
+          />
+          <Experiences 
+          name="Guy" 
+          img={Guy}
+          rating="4.5"
+          reviewCount={20}
+          country= "MX"
+          title="Go for it with "
+          price={150}
+          />
+        </div>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/Experiences.jsx
+++ src/components/Experiences.jsx
@@ -1,20 +1,21 @@
 import React from "react";
-import Morocha from "../assets/images/Morocha.jpg";
+
+
 import Star from "../assets/images/Star.png";
 
-export default function Experiences() {
+export default function Experiences(props) {
   return (
     <div className=" w-44 text-base p-5">
-      <img className="w-full rounded-lg " src={Morocha} alt="Morocha" />
+      <img className="w-full rounded-lg " src={props.img} alt={props.img} />
       <div className="flex items-center justify-around">
         <img className="h-3" src={Star} alt="" />
-        <span> 5.0 </span>
-        <span className="font-thin text-gray-500">(6) * </span>
-        <span className=" text-gray-700 font-bold">USA</span>
+        <span> {props.rating} </span>
+        <span className="font-thin text-gray-500">({props.reviewCount}) *  </span>
+        <span className=" text-gray-700 font-bold">{props.country}</span>
       </div>
-      <p> Life is hard with Morocha</p>
+      <p> {props.title} {props.name}</p>
       <p>
-        <span className="font-semibold">From 136$</span> / person
+        <span className="font-semibold">From {props.price}$</span> / person
       </p>
     </div>
   );
